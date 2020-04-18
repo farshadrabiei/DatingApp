f898e7f584dd3e040256b85f79e59669f18e64bf
farshad rabiei <f.rabiei60@hotmail.com>
2020-04-18 14:07:52 +0430

    ng prime Add

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
- 
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { MenubarModule } from 'primeng/menubar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -10,15 +12,23 @@ import { AuthService } from './_service/auth.service';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorInterceptorProvider } from './_service/error.interceptor';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, RegisterComponent, HomeComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule 
+    FormsModule,
+    MenubarModule,
+    SplitButtonModule,
+    BrowserAnimationsModule,
   ],
-  providers: [AuthService, ErrorInterceptorProvider],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider,
+    { provide: Router, useValue: {} },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_service/auth.service';
 import { AlertifyService } from '../_service/alertify.service';
-
+import { MenuItem } from 'primeng/api/menuitem';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -9,9 +9,23 @@ import { AlertifyService } from '../_service/alertify.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
+  items: MenuItem[];
   constructor(public auth: AuthService, private alertify: AlertifyService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.items = [
+      { styleClass: 'mt-t', label: 'ویرایش پروفایل', icon: 'fa fa-user' },
+      { separator: true },
+      {
+        styleClass: 'mt-t',
+        label: 'خروج',
+        icon: 'fa fa-sign-out',
+        command: (event) => {
+          this.logout();
+        },
+      },
+    ];
+  }
 
   login() {
     this.auth.login(this.model).subscribe(
